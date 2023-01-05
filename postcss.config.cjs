c35684e07f671c306bd2ed38cff1902c0017697d
camilo micheletto <camilomichelettors@gmail.com>
2023-01-05 15:23:35 -0300

    fix: removed configs and styling

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -10,16 +10,6 @@ module.exports = {
       stage: 0,
       autoprefixer: true,
 			enableClientSidePolyfills: false,
-      features: {
-        'logical-properties-and-values': false, 
-        'prefers-color-scheme-query': false, 
-        'gap-properties': false,
-        'custom-properties': false,
-        'dir-pseudo-class': false,
-        'focus-within-pseudo-class': false,
-        'focus-visible-pseudo-class': false,
-        'color-functional-notation': false,
-      }
     }),
     cssnano({ preset: 'default' })
   ]
